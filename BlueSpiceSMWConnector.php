3b8139066c214206926ebe63f4b813a6545693e0
Leonid Verhovskij <l.verhovskij@gmail.com>
2016-09-28 21:46:48 +0200

    Move registration to extension.json
    Change-Id: I87f43d3c8727a68f66a33a2ce65059111ad6ffde

--- BlueSpiceSMWConnector.php
+++ BlueSpiceSMWConnector.php
@@ -4,78 +4,9 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-$GLOBALS['wgExtensionCredits']['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'BSSMWConnector',
-	'author' => '[mailto:vogel@hallowelt.com Robert Vogel (Hallo Welt! Medienwerkstatt GmbH)]',
-	'descriptionmsg' => 'bs-bssmwconnector-desc',
-	'version' => '1.23',
-	'url' => 'https://www.hallowelt.com',
-	'license-name' => 'GPL-2.0+'
-);
-
-$GLOBALS['wgMessagesDirs']['BSSMWConnectorHooks'] = __DIR__ .'/i18n';
-
-$GLOBALS['wgAutoloadClasses']['BSSMWConnectorHooks'] = __DIR__.'/includes/BSSMWConnectorHooks.php';
-$GLOBALS['wgAutoloadClasses']['BSSMWCNamespaceManager'] = __DIR__.'/includes/BSSMWCNamespaceManager.php';
-$GLOBALS['wgAutoloadClasses']['BSSFVisualEditor'] = __DIR__.'/includes/forminputs/BSSFVisualEditor.php';
-$GLOBALS['wgAutoloadClasses']['BSGridResultPrinter'] = __DIR__.'/includes/formats/Grid/BSGridResultPrinter.php';
-$GLOBALS['wgAutoloadClasses']['BSPropertyRegistry'] = __DIR__.'/includes/BSPropertyRegistry.php';
-$GLOBALS['wgAutoloadClasses']['BSDefinitionReader'] = __DIR__.'/includes/BSDefinitionReader.php';
-$GLOBALS['wgAutoloadClasses']['BSExtraPropertyAnnotator'] = __DIR__.'/includes/BSExtraPropertyAnnotator.php';
-
-#$GLOBALS['wgExtensionFunctions'][] = 'BSSMWConnectorHooks::setup';
-$GLOBALS['wgHooks']['BeforePageDisplay'][] = 'BSSMWConnectorHooks::onBeforePageDisplay';
-$GLOBALS['wgHooks']['sfFormPrinterSetup'][] = 'BSSMWConnectorHooks::onSfFormPrinterSetup';
-$GLOBALS['wgHooks']['BSBookshelfNodeTag'][] = 'BSSMWConnectorHooks::onBSBookshelfNodeTag';
-$GLOBALS['wgHooks']['BSBookshelfExportTag'][] = 'BSSMWConnectorHooks::onBSBookshelfExportTag';
-
-$GLOBALS['wgHooks']['NamespaceManager::getMetaFields'][] = 'BSSMWCNamespaceManager::onGetMetaFields';
-$GLOBALS['wgHooks']['NamespaceManager::getNamespaceData'][] = 'BSSMWCNamespaceManager::onGetNamespaceData';
-$GLOBALS['wgHooks']['NamespaceManager::editNamespace'][] = 'BSSMWCNamespaceManager::onEditNamespace';
-$GLOBALS['wgHooks']['NamespaceManager::writeNamespaceConfiguration'][] = 'BSSMWCNamespaceManager::onWriteNamespaceConfiguration';
+wfLoadExtension( 'BlueSpiceSMWConnector' );
 
 $GLOBALS['smwgResultFormats']['bsgrid'] = 'BSGridResultPrinter';
-
-$aResourceModuleTemplate = array(
-	'localBasePath' => $IP.'/extensions/BSSMWConnector/resources',
-	'remoteExtPath' => 'BSSMWConnector/resources',
-);
-
-$GLOBALS['wgResourceModules']['ext.BSSMWConnector.BookshelfUI'] = array(
-	'scripts' => array(
-		'ext.BSSMWConnector.BookshelfUI.js'
-	),
-	'messages' => array()
-) + $aResourceModuleTemplate;
-
-$GLOBALS['wgResourceModules']['ext.BSSMWConnector'] = array(
-	'scripts' => array(
-		'ext.BSSMWConnector.js',
-		'ext.BSSMWConnector.smwc.js',
-		'ext.BSSMWConnector.util.js'
-	),
-	'dependencies' => array(
-		'ext.bluespice.extjs',
-		'ext.smw.api'
-	)
-) + $aResourceModuleTemplate;
-
-$GLOBALS['wgResourceModules']['ext.BSSMWConnector.SF.VisualEditorField'] = array(
-	'scripts' => array(
-		'ext.BSSMWConnector.SF.VisualEditorField.js'
-	)
-) + $aResourceModuleTemplate;
-
-$GLOBALS['wgResourceModules']['ext.srf.bsextjsgrid'] = array(
-	'scripts' => array(
-		'ext.srf.bsextjsgrid.js'
-	),
-	'dependencies' => array(
-		'ext.BSSMWConnector',
-		'ext.smw.api'
-	)
-) + $aResourceModuleTemplate;
 
 /**
  * Dieser Hack ist in der aktuellen Version von BlueSpiceExtensions
