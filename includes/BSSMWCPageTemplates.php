1fac411b77dd4dc9d23b883ae9b2cbe9cf7fad77
rvogel <vogel@hallowelt.biz>
2017-02-24 16:42:38 +0100

    Adding integration into BlueSpicePageTemplates
    Needs  https://gerrit.wikimedia.org/r/339638 to work
    
    NEEDS CHERRY-PICK TO REL1_27
    
    Added package.json and Gruntfile.js to make CI happy
    
    Change-Id: I8cc51e27f7ddd8bdff9ce22f4b5d6e3fc44a6ca8

---
+++ includes/BSSMWCPageTemplates.php
@@ -0,0 +1,29 @@
+<?php
+
+class BSSMWCPageTemplates {
+
+	/**
+	 *
+	 * @param PageTemplates $oSender
+	 * @param BSPageTemplateList $oPageTemplateList
+	 * @param BSPageTemplateListRenderer $oPageTemplateListRenderer
+	 * @param Title $oTitle
+	 * @return boolean
+	 */
+	public static function onBSPageTemplatesBeforeRender( $oSender, &$oPageTemplateList, &$oPageTemplateListRenderer, $oTitle ) {
+		$aData = $oPageTemplateList->getAll();
+		foreach( $aData as $iId => $aDataSet ) {
+			if( (int)$aDataSet['pt_template_namespace'] === SF_NS_FORM ) {
+				$oFormTitle = Title::makeTitle(
+					SF_NS_FORM,
+					$aDataSet['pt_template_title'] . '/' . $oTitle->getPrefixedDBkey()
+				);
+				$oTargetTitle = SpecialPage::getTitleFor( 'FormEdit', $oFormTitle->getDBkey() );
+				$aDataSet['target_url'] = $oTargetTitle->getLinkURL();
+				$oPageTemplateList->set( $iId, $aDataSet );
+			}
+		}
+
+		return true;
+	}
+}
