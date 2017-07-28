a0ce68ea4d27c750c70fb0239a386a62a84ef7e5
rvogel <vogel@hallowelt.biz>
2017-07-28 09:40:06 +0200

    Fix for PageForms DateImePicker
    Extension:PageForms uses 'OutputPage::addModuleScripts' to add modules ot RL.
    Unfortunately this does not resolve dependencies the way
    'OutputPapge::addModules' does.
    
    This is a workaround.
    
    NEEDS CHERRY-PICK TO REL1_27
    
    Change-Id: I2339b259c0a3e525cd83edb5749b8542f7bef57e

---
+++ resources/ext.BSSMWConnector.PageForms.DateTimePicker.fix.js
@@ -0,0 +1,5 @@
+/* 2017-07-03, rvogel: "Workaround für Datepicker in Form:Termin" ERM5255#note-76062 */
+if( mw.config.get('wgCanonicalSpecialPageName') === "FormEdit" ) {
+    mw.loader.load( 'jquery.ui.datepicker' );
+    mw.loader.load( 'ext.pageforms.datepicker' );
+}
