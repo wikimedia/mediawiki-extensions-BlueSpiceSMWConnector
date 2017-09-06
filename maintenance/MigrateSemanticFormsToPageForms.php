b4f374261fd20f85859e352bd88ae20c6360629c
rvogel <vogel@hallowelt.biz>
2017-09-06 14:10:03 +0200

    Added migration scripts for...
    * ... SemanticForms to PageForms
    * ... reset of $smwgNamespaceIndex to default
    
    Change-Id: Ib949c1a9e5f5359b51fd1a2c6d9d8b7bc6d3a6b1
    (cherry picked from commit d718733959870ccfc296b182e8759b697bb9ae9d)

---
+++ maintenance/MigrateSemanticFormsToPageForms.php
@@ -0,0 +1,115 @@
+<?php
+
+require '../../../maintenance/Maintenance.php';
+
+class MigrateSemanticFormsToPageForms extends Maintenance {
+
+	protected $propHasDefaultFormVariants = [
+		'لديه استمارة افتراضية',
+		'Fa servir el formulari per defecte',
+		'Hat Standardformular',
+		'Έχει προεπιλεγμένη φόρμα',
+		'Has default form',
+		'Usa el formulario por defecto',
+		'فرم پیش‌فرض دارد',
+		'Oletuslomake',
+		'Utilise le formulaire',
+		'משתמש בטופס',
+		'Memiliki formulir bawaan',
+		'Usa il modulo predefinito',
+		'Heeft standaard formulier',
+		'Har standardskjema',
+		'预设表单',
+		'預設表單'
+	];
+
+	protected $pagesToChange = [];
+
+	public function execute() {
+		$this->output( "Searching for pages with [[Has default form::+]] ..." );
+		foreach( $this->propHasDefaultFormVariants as $propName ) {
+			$this->executeAsk( $propName );
+		}
+		$count = count( $this->pagesToChange );
+		$this->output( " done.\nFound: " . $count );
+		if( $count === 0 ) {
+			$this->output( "\nNothing to do.\n" );
+			return;
+		}
+
+		$this->output( "\nModifying..." );
+		foreach( $this->pagesToChange as $pageName => $dummyVal ) {
+			$this->replacePropertyByParserFunction( $pageName );
+		}
+		$this->output( "\n... done.\n" );
+	}
+
+	protected function executeAsk( $propName ) {
+		global $wgRequest;
+		 $api = new ApiMain(
+			new DerivativeRequest(
+				$wgRequest,
+				array(
+					'action' => 'askargs',
+					'conditions' => "$propName::+",
+					'parameters' => 'limit=9999'
+				)
+			)
+		);
+
+		$api->execute();
+		$data = $api->getResult()->getResultData();
+
+		if( !empty( $data['query'] ) && !empty( $data['query']['results'] ) ) {
+			foreach( $data['query']['results'] as $pageName => $printout ) {
+				$this->pagesToChange[$pageName] = true;
+			}
+		}
+	}
+
+	protected function replacePropertyByParserFunction( $pageName ) {
+		$this->output( "\n\t$pageName" );
+		$wikiPage = Wikipage::factory( Title::newFromText( $pageName ) );
+		$content = $wikiPage->getContent();
+		if( $content instanceof WikitextContent === false ) {
+			$this->output( "--> No WikiText. Can not modify." );
+			return;
+		}
+		$wikiText = $content->getNativeData();
+		$wikiText = preg_replace_callback(
+			'#\[\[(.*?)::(.*?)\]\]#',
+			function( $matches ) {
+				//Normalize "Has_default form" --> "Has default form"
+				$propName = str_replace( '_', ' ', $matches[1] );
+				if( in_array( $propName, $this->propHasDefaultFormVariants ) ) {
+					return '{{#default_form:' . $this->extractFormName( $matches[2] ).'}}';
+				}
+				return $matches[0];
+			},
+			$wikiText
+		);
+		$status = $wikiPage->doEditContent(
+			ContentHandler::makeContent( $wikiText, $wikiPage->getTitle()),
+			"SemanticForms to PageForms, done by ".self::class
+		);
+
+		if( $status->isOK() ) {
+			$this->output( ' OK.');
+		}
+		else {
+			$this->output( 'Error: ' . $status->getMessage()->plain() );
+		}
+	}
+
+	protected function extractFormName( $formNameAndMaybeAlias ) {
+		//This is not actually neccessary as
+		//{{#default_form:SomeForm|Alias}} (derived from [[Has default from::SomeForm|Alias]])
+		//will work the same as
+		//{{#default_form:SomeForm}}
+		$parts = explode( '|', $formNameAndMaybeAlias );
+		return $parts[0];
+	}
+}
+
+$maintClass = 'MigrateSemanticFormsToPageForms';
+require_once( RUN_MAINTENANCE_IF_MAIN );
