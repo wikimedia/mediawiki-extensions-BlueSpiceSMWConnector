12ffdbb1c7cadc2ded19e64b7ef78f59b99f10a6
dvogel <daniel.vogel@hallowelt.com>
2018-04-13 11:26:28 +0200

    Bugfix: copy&paste from MS Word to form field
    Change-Id: Ib69d0fc3e8f76e55569a797632cef53392dde244

--- resources/ext.BSSMWConnector.SF.VisualEditorField.js
+++ resources/ext.BSSMWConnector.SF.VisualEditorField.js
@@ -13,6 +13,8 @@ mw.loader.using( 'ext.bluespice.visualEditor.tinymce' ).done( function() {
 			//We silently require "InsertLink" ('bslink') extension to also be enabled. This is not nice, but okay for now.
 			toolbar1: 'undo redo | bold italic underline strikethrough | bslink unlink | bullist numlist | outdent indent | styleselect forecolor removeformat',
 			plugins: 'textcolor colorpicker table lists', //We don't use 'table', we just need it for 'bsactions'
+			paste_word_valid_elements: 'b,strong,i,em,h1,h2,h3,h4,h5,table,thead,tfoot,tr,th,td,ol,ul,li,a,sub,sup,strike,br,del,div,p',
+			paste_retain_style_properties: 'color text-decoration text-align',
 			external_plugins: {
 				'bswikicodetemplateunescape': sp + '/extensions/BlueSpiceSMWConnector/resources/tiny_mce_plugins/bswikicodetemplateunescape/plugin.js',
 				'bswikicode': '../tiny_mce_plugins/bswikicode/plugin.js',
