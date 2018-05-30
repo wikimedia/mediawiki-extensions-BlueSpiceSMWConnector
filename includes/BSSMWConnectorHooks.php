2ab890b954245bc75443e3f2f9cb9405bcb92119
Dejan Savuljesku <d.savuljesku@gmail.com>
2018-05-30 09:03:06 +0200

    Fix for bsvisualeditor field in multi templates
    To use this field with multi template we must register function to run
    on each template initalization
    
    ERM; #9659
    
    Change-Id: Ic64aa9ddb6e188177880ba60bbd5c30a3016a8f9

--- includes/BSSMWConnectorHooks.php
+++ includes/BSSMWConnectorHooks.php
@@ -35,7 +35,6 @@ class BSSMWConnectorHooks {
 		$out->addModules( array(
 			'ext.bluespice.visualEditor.styles',
 			'ext.bluespice.visualEditor.tinymce',
-			'ext.BSSMWConnector.SF.VisualEditorField',
 			'ext.BSSMWConnector.SF.FreeTextVisualEditor'
 		) );
 
