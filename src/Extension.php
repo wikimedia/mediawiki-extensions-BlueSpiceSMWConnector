73d617b2ac7f16278515ed78bf625f49da11281d
Peter Boehm <boehm@hallowelt.com>
2019-02-27 17:03:01 +0100

    Add Extension.php to Extension
    Missing Extension.php lead to error of JS loading in bookshelf
    
    Due to ERM#7485
    [3.1]
    
    Change-Id: I634e411ddc3471e06ead44c69dcfa647f081521d

---
+++ src/Extension.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * BlueSpiceSMWConnector extension for BlueSpice
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * This file is part of BlueSpice MediaWiki
+ * For further information visit http://bluespice.com
+ *
+ * @package    BlueSpiceSMWConnector
+ * @subpackage BlueSpiceSMWConnector
+ * @copyright  Copyright (C) 2019 Hallo Welt! GmbH, All rights reserved.
+ * @license    http://www.gnu.org/copyleft/gpl.html GPL-3.0-only
+ * @filesource
+ */
+
+namespace BlueSpice\SMWConnector;
+
+class Extension extends \BlueSpice\Extension {
+
+}
