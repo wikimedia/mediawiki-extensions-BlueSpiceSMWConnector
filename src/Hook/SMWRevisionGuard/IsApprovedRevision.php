7b7f521ee4a6d4103b7ad6bdb1e38274f0c12d76
alexpin <intracomof@gmail.com>
2020-07-21 20:02:19 +0300

    SemanticMediawiki RevisionGuard base hooks
    ERM16819
    
    Change-Id: I795a881b504f0b6306b2e0478e109098a17dfa18

---
+++ src/Hook/SMWRevisionGuard/IsApprovedRevision.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Hook handler base class for SMW:RevisionGuard:IsApprovedRevision hook
+ * (https://doc.semantic-mediawiki.org/md_content_extensions_SemanticMediaWiki_docs_technical_hooks_hook_8revisionguard_8isapprovedrevision.html)
+ */
+namespace BlueSpice\SMWConnector\Hook\SMWRevisionGuard;
+
+use BlueSpice\Hook;
+use Config;
+use IContextSource;
+use Title;
+
+abstract class IsApprovedRevision extends Hook {
+
+	/** @var Title */
+	protected $title;
+
+	/** @var int */
+	protected $latestRevID;
+
+	/**
+	 * @param Title $title
+	 * @param int $latestRevID
+	 * @return bool
+	 */
+	public static function callback( Title $title, $latestRevID ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$title,
+			$latestRevID
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 * SMWRevisionGuardIsApprovedRevision constructor.
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param Title $title
+	 * @param int $latestRevID
+	 */
+	public function __construct( $context, $config, Title $title, $latestRevID ) {
+		parent::__construct( $context, $config );
+		$this->title = $title;
+		$this->latestRevID = $latestRevID;
+	}
+
+}
