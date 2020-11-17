6bd00b5657fa8d550ebf16572166133ecb801e04
pwirth <wirth@hallowelt.biz>
2020-11-17 15:29:36 +0100

    Use lookup modifier factory to make these extendable/removable
    => Requires: I0772c9bc21b2741aed948fb16ee416fba70902de
    
    ERM:21685
    [3.1.x]
    
    Change-Id: I682810c43f3a364ea150cefac190599416037422

--- src/ExtendedSearch/Source/SMWWikiPage.php
+++ src/ExtendedSearch/Source/SMWWikiPage.php
@@ -2,13 +2,9 @@
 
 namespace BlueSpice\SMWConnector\ExtendedSearch\Source;
 
-use BS\ExtendedSearch\Source\WikiPages;
-use BS\ExtendedSearch\Source\LookupModifier\Base as LookupModifier;
 use BlueSpice\SMWConnector\ExtendedSearch\Source;
-use BlueSpice\SMWConnector\ExtendedSearch\Source\LookupModifier\AddSourceFields;
-use BlueSpice\SMWConnector\ExtendedSearch\Source\LookupModifier\AddSMWAggregation;
-use BlueSpice\SMWConnector\ExtendedSearch\Source\LookupModifier\ParseSMWFilters;
 use BlueSpice\SMWConnector\ExtendedSearch\Source\Formatter\SMWWikiPageFormatter;
+use BS\ExtendedSearch\Source\WikiPages;
 
 class SMWWikiPage extends WikiPages {
 	/**
@@ -37,24 +33,6 @@ class SMWWikiPage extends WikiPages {
 		return new Source\MappingProvider\SMWWikiPage(
 			$this->oDecoratedSource->getMappingProvider()
 		);
-	}
-
-	/**
-	 *
-	 * @param \BS\ExtendedSearch\Lookup $oLookup
-	 * @param \IContextSource $oContext
-	 * @param string $sType
-	 * @return ParseSMWFilters
-	 */
-	public function getLookupModifiers( $oLookup, $oContext, $sType = LookupModifier::TYPE_SEARCH ) {
-		$lookupModifiers = parent::getLookupModifiers( $oLookup, $oContext, $sType );
-
-		if ( $sType === LookupModifier::TYPE_SEARCH ) {
-			$lookupModifiers['smw-source-fields'] = new AddSourceFields( $oLookup, $oContext );
-			$lookupModifiers['smw-aggregation'] = new AddSMWAggregation( $oLookup, $oContext );
-			$lookupModifiers['smw-filter-parser'] = new ParseSMWFilters( $oLookup, $oContext );
-		}
-		return $lookupModifiers;
 	}
 
 	/**
