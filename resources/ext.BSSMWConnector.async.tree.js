e342eb4206bd1c879984a8f7ffd64b26268f5bfa
Dejan Savuljesku <savuljesku@hallowelt.com>
2021-01-26 14:29:45 +0100

    Add loading text to AsyncTree
    [3.1.x]
    
    ERM 21798
    
    Change-Id: I1cff7c5b827f88acb180c437ef54081818123c99

--- resources/ext.BSSMWConnector.async.tree.js
+++ resources/ext.BSSMWConnector.async.tree.js
@@ -5,11 +5,14 @@
 			var data = $container.data( 'query' );
 
 			Ext.require( 'BS.SMWConnector.tree.AsyncResultPrinter', function() {
-				Ext.create( 'BS.SMWConnector.tree.AsyncResultPrinter', {
+				var tree = Ext.create( 'BS.SMWConnector.tree.AsyncResultPrinter', {
 					renderTo: $container[0],
 					query: data.query || '',
 					rootNode: data.rootNode || '',
 					storeAction: data.storeAction
+				} );
+				tree.store.on( 'load', function() {
+					$container.find( '.loading-text' ).remove();
 				} );
 			} );
 		} );
