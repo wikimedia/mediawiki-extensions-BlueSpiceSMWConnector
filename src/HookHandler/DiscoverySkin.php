02f1fb2d5cdcbe2650afe58b8f885ab71b10bb73
pwirth <wirth@hallowelt.biz>
2021-09-23 14:31:49 +0200

    Move "Browse properties" to details panel in discovery skin
    ERM:25778
    [4.x]
    
    Change-Id: I816e9c9e4742154d23c033583fe2cb0e49ded4df

---
+++ src/HookHandler/DiscoverySkin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BlueSpice\SMWConnector\HookHandler;
+
+use BlueSpice\Discovery\Hook\BlueSpiceDiscoveryTemplateDataProviderAfterInit;
+use BlueSpice\Discovery\ITemplateDataProvider;
+
+class DiscoverySkin implements BlueSpiceDiscoveryTemplateDataProviderAfterInit {
+
+	/**
+	 *
+	 * @param ITemplateDataProvider $registry
+	 * @return void
+	 */
+	public function onBlueSpiceDiscoveryTemplateDataProviderAfterInit( $registry ): void {
+		$registry->unregister( 'toolbox', 't-smwbrowselink' );
+		$registry->register( 'actions_details', 't-smwbrowselink' );
+	}
+}
