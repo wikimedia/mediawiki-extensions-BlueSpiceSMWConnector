10a05556b591b3d0529385321b05880d64573ad1
Hamish Slater <hamishslater@hotmail.com>
2023-04-21 14:02:38 +0200

    Implement dataquery tag
    Move SMWReport from BSProDistributionConnector
    Add inspector
    
    ERM32394
    
    [REL1_39, master]
    
    Depends-On: I5fe91e8e63d65c4b4eca5b4d9c454958b8febd2b
    
    Change-Id: I423805051f76617425c048d9cd25bf49f83128d3

---
+++ src/ContentDroplets/DataQueryDroplet.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace BlueSpice\SMWConnector\ContentDroplets;
+
+use MediaWiki\Extension\ContentDroplets\Droplet\TagDroplet;
+use Message;
+
+class DataQueryDroplet extends TagDroplet {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getName(): Message {
+		return Message::newFromKey( 'bs-smwconnector-dataquery-name' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getDescription(): Message {
+		return Message::newFromKey( 'bs-smwconnector-dataquery-desc' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIcon(): string {
+		return 'droplet-smw-dataquery';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRLModules(): array {
+		return [
+			'ext.BSSMWConnector.DataQuery.visualEditor'
+		];
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getCategories(): array {
+		return [ 'lists', 'data', 'navigation' ];
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getTagName(): string {
+		return 'dataquery';
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getAttributes(): array {
+		return [];
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function hasContent(): bool {
+		return false;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getVeCommand(): ?string {
+		return 'dataqueryCommand';
+	}
+}
---
+++ src/Tag/DataQuery.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace BlueSpice\SMWConnector\Tag;
+
+use BlueSpice\SmartList\BlueSpiceSmartListModeFactory;
+use BlueSpice\SmartList\Mode\IMode;
+use BlueSpice\SmartList\Tag\SmartListHandler;
+use BlueSpice\Tag\Tag;
+use MediaWiki\MediaWikiServices;
+use Parser;
+use PPFrame;
+use RequestContext;
+
+class DataQuery extends Tag {
+
+	/** @var MediaWikiServices */
+	private $services;
+
+	/** @var BlueSpiceSmartListModeFactory */
+	private $factory;
+
+	/** @var IMode */
+	private $mode;
+
+	public function __construct() {
+		$this->services = MediaWikiServices::getInstance();
+		$this->factory = $this->services->getService( 'BlueSpiceSmartList.SmartlistMode' );
+		$this->mode = $this->factory->createMode( 'dataquery' );
+	}
+
+	/**
+	 * @param string $processedInput
+	 * @param array $processedArgs
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @return PageBreakHandler
+	 */
+	public function getHandler( $processedInput, array $processedArgs, Parser $parser, PPFrame $frame ) {
+		$context = RequestContext::getMain();
+		$titleFactory = $this->services->getTitleFactory();
+		$hookContainer = $this->services->getHookContainer();
+
+		return new SmartListHandler(
+			$processedInput,
+			$processedArgs,
+			$parser,
+			$frame,
+			$context,
+			$titleFactory,
+			$hookContainer,
+			$this->mode
+		);
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getTagNames() {
+		return [
+			'dataquery'
+		];
+	}
+
+	/**
+	 * @return IParamDefinition[]
+	 */
+	public function getArgsDefinitions() {
+		return $this->mode->getParams();
+	}
+}
