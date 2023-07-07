610cb2f76e96879be457bca89be9d943143455c8
d.savuljesku <d.savuljesku@gmail.com>
2023-07-07 14:24:33 +0200

    Handle usernames as values of SMW properties when anonymizing and deleting users
    This will handle only links to user pages, not usernames in random string values
    
    [4.3.x]
    
    ERM31974
    
    Change-Id: Ib1eca38996d555367692647cf32bea96af674484
    (cherry picked from commit 8a1f7a015060b9dd04629bcd97b10bf69b870994)

---
+++ src/Privacy/Handler.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace BlueSpice\SMWConnector\Privacy;
+
+use BlueSpice\Privacy\IPrivacyHandler;
+use Status;
+use User;
+use Wikimedia\Rdbms\IDatabase;
+
+class Handler implements IPrivacyHandler {
+	/** @var IDatabase */
+	protected $db;
+
+	/**
+	 *
+	 * @param IDatabase $db
+	 */
+	public function __construct( IDatabase $db ) {
+		$this->db = $db;
+	}
+
+	/**
+	 *
+	 * @param string $oldUsername
+	 * @param string $newUsername
+	 * @return Status
+	 */
+	public function anonymize( $oldUsername, $newUsername ) {
+		$this->db->update(
+			'smw_object_ids',
+			[ 'smw_title' => $newUsername, 'smw_sortkey' => $newUsername, 'smw_sort' => $newUsername ],
+			[
+				// Just handle links to user pages, not random string that match the username
+				'smw_title' => $oldUsername,
+				'smw_namespace' => NS_USER
+			]
+		);
+		return Status::newGood();
+	}
+
+	/**
+	 *
+	 * @param User $userToDelete
+	 * @param User $deletedUser
+	 * @return Status
+	 */
+	public function delete( User $userToDelete, User $deletedUser ) {
+		return $this->anonymize( $userToDelete->getName(), $deletedUser->getName() );
+	}
+
+	/**
+	 *
+	 * @param array $types
+	 * @param string $format
+	 * @param User $user
+	 * @return Status
+	 */
+	public function exportData( array $types, $format, User $user ) {
+		return Status::newGood( [] );
+	}
+}
