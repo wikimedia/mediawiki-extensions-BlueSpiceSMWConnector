b0a1174017d2e74f5a9c879b65a70256faff694a
Mschlindwein <schlindwein@hallowelt.com>
2024-05-27 15:16:33 +0200

    Remove unused book metadata integration
    ERM37318
    
    [4.5.x]
    
    Change-Id: I7f9211b49552ff27e4c4e134f108ac01501dfff4
    (cherry picked from commit b2d8354f329d56728b345ec12e3fc0db24360447)

--- resources/ext.BSSMWConnector.BookshelfUI.js
+++
@@ -1,9 +0,0 @@
-( function( mw, jQuery, bs ) {
-	Ext.onReady(function(){
-		mw.hook( 'ext.bookshelfui.addmass.create' ).add( function( dialog ){
-			var cbSMWProperty = Ext.create( 'BS.SMWConnector.field.SMWCombo' );
-
-			dialog.registerSource( 'smwproperty', 'bs-dlg-choosesmwprop-type-smwprop', cbSMWProperty );
-		});
-	});
-})( mediaWiki, jQuery, blueSpice );
